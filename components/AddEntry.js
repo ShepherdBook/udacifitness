67e21a71e03402808066c4cc56b19a714bd01a70
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-04-18 09:45:25 -0400

    feat: Increment, Decrement, Slide functions.
    Includes local state to track metric values.

--- components/AddEntry.js
+++ components/AddEntry.js
@@ -3,6 +3,47 @@ import { View, Text } from 'react-native'
 import { getMetricMetaInfo } from '../utils/helpers'
 
 export default class AddEntry extends Component {
+
+  state ={
+    run: 0,
+    bike: 0,
+    swim: 0,
+    sleep: 0,
+    eat: 0,
+  }
+
+  increment(metric) {
+    const { max, step } = getMetricMetaInfo(metric)
+
+    this.setState((state) => {
+      const newCount = state[metric] + step
+
+      return {
+        ...state,
+        [metric]: newCount > max ? max : newCount
+      }
+    })
+  }
+
+  decrement(metric) {
+    const { step } = getMetricMetaInfo(metric)
+
+    this.setState((state) => {
+      const newCount = state[metric] - step
+
+      return {
+        ...state,
+        [metric]: newCount < 0 ? 0 : newCount
+      }
+    })
+  }
+
+  slide(metric, value) {
+    this.setState(() => ({
+      [metric]: value
+    }))
+  }
+
   render() {
     return (
       <View>

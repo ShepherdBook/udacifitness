5f2e5a979155e4d5a7114e8c29aeb22e844ddf19
Zach Johnston <5273704+zachjohnston@users.noreply.github.com>
2019-04-22 19:30:39 -0400

    feat: Navigation via a tab bar

--- App.js
+++ App.js
@@ -5,15 +5,56 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducer from "./reducers";  // i.e. the default export from ./reducers/index.js
 import History from './components/History';
+import { createBottomTabNavigator, createAppContainer } from "react-navigation";
+import { purple, white } from "./utils/colors";
+import { FontAwesome, Ionicons } from "@expo/vector-icons";
+import { Platform } from 'expo-core';
+
+const Tabs = createBottomTabNavigator({
+  History: {
+    screen: History,
+    navigationOptions: {
+      tabBarLabel: 'History',
+      tabBarIcon: ({ tintColor }) => <Ionicons name='ios-bookmarks' size={30} color={tintColor} />
+    }
+  },
+  AddEntry: {
+    screen: AddEntry,
+    navigationOptions: {
+      tabBarLabel: 'Add Entry',
+      tabBarIcon: ({ tintColor }) => <FontAwesome name='plus-square' size={30} color={tintColor} />
+    }
+  }
+}, {
+  navigationOptions: {
+    header: null
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0,0,0,0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1
+    }
+  }
+})
+
+// Required by React Navigation v3
+const AppContainer = createAppContainer(Tabs)
 
 export default class App extends React.Component {
-
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}}>
           <View style={{height: 20}}></View>
-          <History />
+          <AppContainer />
         </View>
       </Provider>
     )
